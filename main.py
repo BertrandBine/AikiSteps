c43108b75c4fa73125c08c67acd8b26682b21659
Bertrand Bine <bertrand.bine@orange.fr>
2018-06-24 19:30:51 +0200

    Signed-off-by: Bertrand Bine <bertrand.bine@orange.fr>

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ from kivy.app import App
 from controleurs.gestion_cours import Gestion_Cours
 from vues.detail_cours_view import Detail_Cours_View
 
-
 class AikiSteps2App(App):
     ##classe pour tester kivy
     def build(self):
         self.gestion_cours=Gestion_Cours()
         self.DCV=Detail_Cours_View()
         return self.DCV
-        
         
 if __name__ == '__main__': 
     AikiSteps2App().run()
--- vues/detail_cours_view.py
+++ vues/detail_cours_view.py
@@ -25,17 +25,14 @@ try:
 except:
     pass
 
-
 Builder.load_file('./vues/detail_cours_view.kv')
-
 
 class CarouselSlide(BoxLayout):
     def build(self):
         return self
-    
+
     def get_le_cours_details(self, instance):
         return self.root.get_cours_data.COURS_STEP[self.index]
-
 
 class LFontInput(TextInput):
     def build(self):
@@ -50,22 +47,22 @@ class DurTot(Label):
 
 class RightButton(Button):
     def build(self):
-        self.first_time = True
         return self
-
+    
 class AddButton(Button):
-    def add_page(self, instance, dcv):
-        dcv.le_cours.COURS_STEPS.insert(instance.index+1, Cours_Steps())
-        instance.add_widget(CarouselSlide(allow_stretch=True,))
-        instance.load_next()
+    def add_page(self, instance):
+        CS=Cours_Steps()
+        instance.details.append(CS)
+        instance.add_widget(CarouselSlide(allow_stretch=True, details=CS))
+        instance.load_slide(instance.slides[-1])
+    pass
 
 class DelButton(Button):
-    def del_page(self, instance, dcv):
-        del dcv.le_cours.COURS_STEPS[instance.index]
-        instance.remove_widget(instance.current_slide)
+    def build(self):
+        return self
 
 class Detail_Cours_View(Screen):
-    ##classe pour tester kivy
+    ##vue principale kivy
     def __init__(self, **kwargs):
         super(Detail_Cours_View, self).__init__(**kwargs)
     
@@ -92,76 +89,83 @@ class Detail_Cours_View(Screen):
             self.fields = None
         self.prepare()
 
-    def get_car(self, thecar=0):
-        i=[i for i in self.root.walk() if type(i) == Carousel]
-        return i[thecar]
+    # def get_car(self, thecar=0):
+    #     #recherche le Carousel 0
+    #     i=[i for i in self.root.walk() if type(i) == Carousel]
+    #     return i[thecar]
 
-    def summerize(self):
-        ET=self.entete()
-        DE=self.detail()
-        result=''
-        dbrow=[]
-        RUB_ET=['Thematique', \
-                'Date', \
-                'Objectif cours', \
-                'Duree', \
-                'Lieu', \
-                'Public1', \
-                'Public2']
-        for i in ET:
-            dbrow.append(i)
-            theindex=ET.index(i)
-            try:
-                print i[0].thefield
-            except:
-                pass
-            if RUB_ET[theindex] != 'Public2':
-                titre=RUB_ET[theindex]
-                if titre == 'Public1': titre = 'Public'
-                result += titre + ' : ' + i[1] +'\n'
-            else:
-                try:
-                    result = result[:-2] + ' (' + i[1] + ')\n'
-                except:
-                    pass
-        result += '=='*10 + '\n'
-        for i in DE[1:-1]:
-            dbrow.append(i)
-            if 'mn.' in i[1]: result += '\n'
-            result += capitalize(i[0].thefield)+ ": " + i[1] + '\n'
+    # def summerize(self):
+    #     #résume le cours en un texte
+    #     ET=self.entete()
+    #     DE=self.detail()
+    #     result=''
+    #     dbrow=[]
+    #     RUB_ET=['Thematique', \
+    #             'Date', \
+    #             'Objectif cours', \
+    #             'Duree', \
+    #             'Lieu', \
+    #             'Public1', \
+    #             'Public2']
+    #     for i in ET:
+    #         dbrow.append(i)
+    #         theindex=ET.index(i)
+    #         try:
+    #             print i[0].thefield
+    #         except:
+    #             pass
+    #         if RUB_ET[theindex] != 'Public2':
+    #             titre=RUB_ET[theindex]
+    #             if titre == 'Public1': titre = 'Public'
+    #             result += titre + ' : ' + i[1] +'\n'
+    #         else:
+    #             try:
+    #                 result = result[:-2] + ' (' + i[1] + ')\n'
+    #             except:
+    #                 pass
+    #     result += '=='*10 + '\n'
+    #     for i in DE[1:-1]:
+    #         dbrow.append(i)
+    #         if 'mn.' in i[1]: result += '\n'
+    #         result += capitalize(i[0].thefield)+ ": " + i[1] + '\n'
         
-        if self.db != None:
-            for i in dbrow:
-                print i[0].thefield," : ", i[1]
+    #     if self.db != None:
+    #         for i in dbrow:
+    #             print i[0].thefield," : ", i[1]
         
-        return result
+    #     return result
 
-    def get_texts(self,the_slides):
-        result=[]
-        wids=[]
-        for i in the_slides:
-            for j in i.walk():
-                try:
-                    if (j not in wids) and ((type(j) not in [Label, Button, ToggleButton, AddButton, DelButton]) or (type(j) == ToggleButton and j.state == 'down') or (' mn.' in j.text)):
-                        result.append((j, j.text))
-                        wids.append(j)
-                except:
-                    pass
-        return result
+    # def get_texts(self,the_slides):
+    #     #récupère les textes des slides
+    #     result=[]
+    #     wids=[]
+    #     for i in the_slides:
+    #         for j in i.walk():
+    #             try:
+    #                 if (j not in wids) and ((type(j) not in [Label, Button, ToggleButton, AddButton, DelButton]) or (type(j) == ToggleButton and j.state == 'down') or (' mn.' in j.text)):
+    #                     result.append((j, j.text))
+    #                     wids.append(j)
+    #             except:
+    #                 pass
+    #     return result
 
-    def entete(self):
-        return self.get_texts(self.get_car(0).slides[0:1])[:]
+    # def entete(self):
+    #     #récupère l'en-tête du cours
+    #     return self.get_texts(self.get_car(0).slides[0:1])[:]
 
-    def detail(self):
-        return self.get_texts(self.get_car(1).slides[:])[:]
+    # def detail(self):
+    #     #récupère le détail du cours
+    #     return self.get_texts(self.get_car(1).slides[:])[:]
 
-    def select_text(self, instance):
-        instance.select_text(0, len(instance.text))
-        instance.cut()
-        instance.delete_selection()
-        instance.text = ''
+    # def select_text(self, instance):
+    #     #selection le texte et le supprime
+    #     instance.select_text(0, len(instance.text))
+    #     instance.cut()
+    #     instance.delete_selection()
+    #     instance.text = ''
     
     def get_cours_data(self):
+        #charge le cours
         self.app=App.get_running_app()
         self.app.gestion_cours.load_cours()
         le_cours=self.app.gestion_cours.liste_cours[0]
